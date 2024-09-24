9f552adb6360d40fdba8b1b1d0dad0e66d915ac0
renanbmello <renanbmello@gmail.com>
2024-09-24 09:58:24 -0300

    web service api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', 
+  // baseURL: 'http://localhost:5000', 
+  baseURL: 'https://backtest-stock.onrender.com', 
+
 });
 
 export default api;
